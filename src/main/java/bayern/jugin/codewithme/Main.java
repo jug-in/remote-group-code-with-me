a7f4550626b4b1eaff0cee494733047da3e43daa
Florian Heubeck <heubeck@mediamarktsaturn.com>
2021-05-11 13:44:19 +0200

    Code-With-Me Projektsetup

---
+++ src/main/java/bayern/jugin/codewithme/Main.java
@@ -0,0 +1,9 @@
+package bayern.jugin.codewithme;
+
+public class Main {
+
+    public static void main(String... args) {
+
+    }
+    
+}
---
+++ src/test/java/bayern/jugin/codewithme/MainTest.java
@@ -0,0 +1,13 @@
+package bayern.jugin.codewithme;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testMainInstanziation() {
+        var main = new Main();
+        assert main != null;
+    }
+
+}
